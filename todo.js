b7ce0cfee943300f864e4cd61a1f4589cb93543d
thiagomayrink <thiagoscmpaiva@gmail.com>
2021-06-07 20:36:23 -0300

    list todos, check/uncheck, remove todos

--- todo.js
+++ todo.js
@@ -3,23 +3,40 @@ import readlineSync from 'readline-sync';
 import { existsSync, writeFileSync, readFileSync } from 'fs';
 
 if (existsSync('./todos.json')){
-    const todoList = readFileSync('./todos.json',{encoding:'utf8'})
+    const todoList = JSON.parse(readFileSync('./todos.json',{encoding:'utf8'}))
     App(todoList)
 } else {
     const todoList = []
-    App(todoList);    
+    App(todoList);
 }
 
 function App(todoList) {
     function addItem(item) {
-        const todo = { status: 'unCheck', name: "" };
+        const todo = { check: false, name: "" };
         todo.name = item.toString();
         todoList = [...todoList, todo];
         writeFileSync('./todos.json', JSON.stringify(todoList, null, 4), 'utf-8');
     }
-    
+    function renderTodoList() {
+        const myTodoList = todoList.map((t)=>{
+            if (t.check === false){
+                return `🔴 ${t.name}`;
+            } else if (t.check === true){
+                return `🟢 ${t.name}`;
+            }
+        })
+        return myTodoList;
+        console.log(myTodoList)
+    }
+
     function removeItem(item) {
         todoList.splice(item)
+        writeFileSync('./todos.json', JSON.stringify(todoList, null, 4), 'utf-8');
+    }
+
+    function toggleCheck(index) {
+        todoList[index].check = !todoList[index].check;
+        writeFileSync('./todos.json', JSON.stringify(todoList, null, 4), 'utf-8');
     }
     
     if (todoList){
@@ -27,16 +44,18 @@ function App(todoList) {
         const index = readlineSync.keyInSelect(functions, 'Type your command? ');
         
         if (functions[index] === 'add') {
-            const item = readlineSync.question('What do you want to do? ');
+            renderTodoList()
+            const item = readlineSync.question('What task do you want to do? ');
             addItem(item)
         }
         if (functions[index] === 'list') {
-            todoList.forEach(e => {
-              console.log(e);
-            });
+            renderTodoList()
+            const index = parseInt(readlineSync.keyInSelect(renderTodoList(), 'What task do you want to check/uncheck? '))
+            toggleCheck(index);
         }
         if (functions[index] === 'remove') {
-            const item = readlineSync.keyInSelect(todoList,'What task do you want to remove? ');
+            renderTodoList()
+            const item = readlineSync.keyInSelect(renderTodoList(),'What task do you want to remove? ');
             removeItem(item);
         }
     }
